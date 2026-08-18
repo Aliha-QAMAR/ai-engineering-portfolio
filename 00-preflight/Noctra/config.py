dc7f66a8686a6ca0768097f5c2b44386dc5b6781
ALIHA QAMAR <qamaraliha07@gmail.com>
2026-08-18 23:49:34 +0500

    Update config.py

--- 00-preflight/Noctra/config.py
+++ 00-preflight/Noctra/config.py
@@ -1,10 +1,1 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'noctra-dev-key-change-in-production')
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-    DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///noctra.db')
-    FLASK_ENV = os.environ.get('FLASK_ENV', 'development')
